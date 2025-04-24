df5a33f984695eedc4d6685d265b7d1645581dff
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-24 20:09:53 +0300

    Update script

--- js/script.js
+++ js/script.js
@@ -236,19 +236,19 @@ window.addEventListener("DOMContentLoaded", function () {
 
       const formData = new FormData(form);
 
-      // const object = {};
-      // formData.forEach(function (value, key) {
-      //   object[key] = value;
-      // });
-      // const json = JSON.stringify(object);
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
 
       fetch("server.php", {
         method: "POST",
-        // headers: {
-        //   "Content-type": "application/json",
-        // },
-        body: formData
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object)
       })
+      .then(data => data.text())
       .then((data) => {
         console.log(data);
         showThanksModal(message.success);
