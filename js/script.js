35e69a8b80866467d865d33f0bba597a8cceeca1
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-26 11:57:40 +0300

    fix error

--- js/script.js
+++ js/script.js
@@ -285,9 +285,9 @@ window.addEventListener("DOMContentLoaded", function () {
       closeModal();
     }, 4000);
   }
-
+  
+  fetch('db.json')
+  .then(data => data.json())
+  .then(res => console.log(res));
 
 });
-fetch('db.json')
-  .then(data => db.json())
-  .then(res => console.log(res));
