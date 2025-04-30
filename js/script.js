1f356f6b98fc3401645a9725675fd56b9d5735f3
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-30 19:54:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -286,10 +286,8 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	// Slider
 
-	let offset = 0;
-	let slideIndex = 1;
-
 	const slides = document.querySelectorAll(".offer__slide"),
+		slider = this.document.querySelector('.offer__slider'),
 		prev = document.querySelector(".offer__slider-prev"),
 		next = document.querySelector(".offer__slider-next"),
 		total = document.querySelector("#total"),
@@ -297,6 +295,9 @@ window.addEventListener("DOMContentLoaded", function () {
 		slidesWrapper = document.querySelector(".offer__slider-wrapper"),
 		width = window.getComputedStyle(slidesWrapper).width,
 		slidesField = document.querySelector(".offer__slider-inner");
+
+		let offset = 0;
+		let slideIndex = 1;
 
 	if (slides.length < 10) {
 		total.textContent = `0${slides.length}`;
@@ -315,6 +316,45 @@ window.addEventListener("DOMContentLoaded", function () {
 	slides.forEach((slide) => {
 		slide.style.width = width;
 	});
+
+	slider.style.position = 'relative';
+
+	const indicators = this.document.createElement('ol');
+	indicators.classList.add('carousel-indicators');
+	indicators.style.cssText = `
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 15;
+		display: flex;
+		justify-content: center;
+		margin-right: 15%;
+		margin-left: 15%;
+		list-style: none;
+	`;
+	slider.append(indicators);
+
+	for (let i = 0; i < slides.length; i++) {
+		const dot = this.document.createElement('li');
+		dot.setAttribute('data-slide-to', i + 1);
+		dot.style.cssText = `
+			box-sizing: content-box;
+			flex: 0 1 auto;
+			width: 30px;
+			height: 6px;
+			margin-right: 3px;
+			margin-left: 3px;
+			cursor: pointer;
+			background-color: #fff;
+			background-clip: padding-box;
+			border-top: 10px solid transparent;
+			border-bottom: 10px solid transparent;
+			opacity: .5;
+			transition: opacity .6s ease;
+		`;
+		indicators.append(dot);
+	}
 
 	next.addEventListener("click", () => {
 		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
